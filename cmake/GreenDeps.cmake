dae8788616e0ae24d7b049c6fe7c737146c34834
Gaurav Harsha <gauravharsha05@gmail.com>
2024-07-28 10:55:09 -0400

    support for custom branch in cmake build deps

--- cmake/GreenDeps.cmake
+++ cmake/GreenDeps.cmake
@@ -6,10 +6,11 @@ function(add_green_dependency TARGET)
     list(LENGTH extra_args extra_count)
     if (${extra_count} GREATER 0)
         list(GET extra_args 0 source)
+        list(GET extra_args 1 custom_release)
         FetchContent_Declare(
             ${TARGET}
             GIT_REPOSITORY ${source}
-            GIT_TAG ${GREEN_RELEASE} # or a later release
+            GIT_TAG ${custom_release} # or a later release
         )
     else()
         FetchContent_Declare(
