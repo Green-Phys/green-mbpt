63cda2d55701df8b55f0c33a9854fdb9145926ef
Emanuel Gull <egull@umich.edu>
2024-10-25 08:52:28 -0400

    chunk reader fixed

--- include/chunk_reader.hpp
+++ include/chunk_reader.hpp
@@ -1,23 +1,46 @@
 #pragma once
 #include<string>
 #include<Eigen/Dense>
+#include<hdf5.h>
 
 class chunk_reader{
   public:
-    chunk_reader(){}
-    chunk_reader(const std::string &basepath, int number_of_keys, int element_size):
+    chunk_reader(){
+      validate_threadsafety();
+    }
+    chunk_reader(const std::string &basepath, int number_of_keys, int naux, int nao):
      basepath_(basepath),
      number_of_keys_(number_of_keys),
-     element_size_(element_size){
+     element_size_(naux*nao*nao*2),
+     naux_(naux),
+     nao_(nao){
+      validate_threadsafety();
       parse_meta();
     }
     const Eigen::VectorXi &chunk_indices() const{ return chunk_indices_;}
     int number_of_keys() const{ return number_of_keys_;}
     int element_size() const{return element_size_;}
+    void validate_threadsafety(){
+      hbool_t is_ts;
+      H5is_library_threadsafe (&is_ts);
+      if(!is_ts) throw std::runtime_error("this hdf5 library is not threadsafe. Threadsafety is a prerequisite for accessing files multiple times");
+    }
+
+    //read key ('chunk') into buffer
+    void read_key(int key, double *buffer);
   private:
     void parse_meta();
+    //figure out where we stored the data (due to manual chunking it may be distributed)
+    void find_file_and_offset(int key, std::string &path, int &chunk_name, unsigned long long &offset);
+    void read_key_at_offset(const std::string &filepath, int chunk_name, unsigned long long offset, double *buffer);
     const std::string basepath_;
     Eigen::VectorXi chunk_indices_;
-    int number_of_keys_;
-    int element_size_;
+    unsigned long long number_of_keys_;
+
+    //total size of a chunk: naux*nao*nao*2
+    unsigned long long element_size_;
+    //number of aux orbitals, dimension1 in hdf5
+    unsigned long long naux_;
+    //number of ao, dimension2 and dimension3(/2) in hdf5 
+    unsigned long long nao_;
 };
--- test/test_chunk_reader.cpp
+++ test/test_chunk_reader.cpp
@@ -9,9 +9,32 @@ TEST(chunk_reader, Init) {
 }
 //these tests are straight from the Si 6x6x6 example
 TEST(chunk_reader, InitBasepath) {
-  chunk_reader c(HDF5_DATA_DIR, 12096, 2163206); //test these numbers
+
+  int chunks_per_file=336;
+  int total_files=36;
+  int nao=26;
+  int naux=200;
+  int number_of_keys=chunks_per_file*total_files;
+
+  chunk_reader c(HDF5_DATA_DIR, number_of_keys, naux, nao); //test these numbers
 
   EXPECT_EQ(c.chunk_indices()[0],0);
   EXPECT_EQ(c.chunk_indices()[1],336);
   EXPECT_EQ(c.chunk_indices()[35],11760);
+}
+TEST(chunk_reader, ReadSomething){
+  int chunks_per_file=336;
+  int total_files=36;
+  int nao=26;
+  int naux=200;
+  int number_of_keys=chunks_per_file*total_files;
+
+  chunk_reader c(HDF5_DATA_DIR, number_of_keys, naux, nao); //test these numbers
+
+  Eigen::VectorXd data(c.element_size());
+  c.read_key(0, &(data[0]));
+  EXPECT_NEAR(data[0], 5.26945, 1.e-5);
+  c.read_key(335, &(data[0]));
+  c.read_key(336, &(data[0]));
+  c.read_key(2000, &(data[0]));
 }
