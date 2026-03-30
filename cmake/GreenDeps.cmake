3803747f8a80681356ca4f2d2aef9379738e3128
Gaurav Harsha <gauravharsha05@gmail.com>
2026-03-30 14:30:15 -0400

    fix dependency sequence in synchronized rlease

--- cmake/GreenDeps.cmake
+++ cmake/GreenDeps.cmake
@@ -6,6 +6,7 @@ function(add_green_dependency TARGET)
             ${TARGET}
             GIT_REPOSITORY https://github.com/Green-Phys/${TARGET}.git
             GIT_TAG ${GREEN_RELEASE} # or a later release
+            CMAKE_ARGS -DGREEN_RELEASE=${GREEN_RELEASE}
     )
 
     FetchContent_MakeAvailable(${TARGET})
